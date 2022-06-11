3bcb624d414918833e2e969c85ff77c261cb3145
TanabeHitoshi <tanabe2852@yahoo.co.jp>
2022-06-11 19:24:46 +0900

    モニタを訂正

--- custom.ts
+++ custom.ts
@@ -79,7 +79,7 @@ namespace custom {
     export function モニタ(): void {
         serial.writeNumbers([pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P0) , Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100), Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100)])
 //        serial.writeValue("postion", pins.analogReadPin(AnalogPin.P1) - pins.analogReadPin(AnalogPin.P0))
-        serial.writeValue("postion", Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100) - Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100))
+        serial.writeValue("postion",  Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100) - Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100))
         serial.writeLine("------------------------------------------------------------------------")
         basic.pause(100)
     }
