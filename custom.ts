b50da47cf29837d2d59e42f433e38a8bb4e9c9d6
Tanabe <tanabe2852@yahoo.co.jp>
2022-06-11 13:04:36 +0900

    標本化

--- custom.ts
+++ custom.ts
@@ -1,6 +1,6 @@
 
 /**
-* ４センサーバージョン
+* 2センサーバージョン
 * このファイルを使って、独自の関数やブロックを定義してください。
 * 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
 */
@@ -70,8 +70,10 @@ namespace custom {
     }
     //% block
     export function モニタ(): void {
-        serial.writeNumbers([pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P0)])
-        serial.writeValue("postion", pins.analogReadPin(AnalogPin.P1) - pins.analogReadPin(AnalogPin.P0))
+        serial.writeNumbers([pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P0), Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100)
+            , Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100)])
+//        serial.writeValue("postion", pins.analogReadPin(AnalogPin.P1) - pins.analogReadPin(AnalogPin.P0))
+        serial.writeValue("postion", Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100) - Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100))
         serial.writeLine("------------------------------------------------------------------------")
         basic.pause(100)
     }
@@ -111,8 +113,8 @@ namespace custom {
     export function ライン位置(): number {
      
         //センサ値の標準化
-        let L_sensor_std = Math.map(pins.analogReadPin(AnalogPin.P0), Left_Senser_White, Left_Senser_Black, 0, 100)
-        let R_sensor_std = Math.map(pins.analogReadPin(AnalogPin.P1), Right_Senser_White, Right_Senser_Black, 0, 100)
+        let L_sensor_std = Math.map(pins.analogReadPin(AnalogPin.P1), Left_Senser_White, Left_Senser_Black, 0, 100)
+        let R_sensor_std = Math.map(pins.analogReadPin(AnalogPin.P0), Right_Senser_White, Right_Senser_Black, 0, 100)
         
         let pos = R_sensor_std - L_sensor_std
 
